ffdb1e0a48cd846a132fe9b174213721d43c7250
ST10381843-Uriel-Lusanda-Mduduzi-Shai <ST10381843@myemeris.edu.za>
2026-05-20 23:23:12 +0200

    6th commit
    Added My units tests for my Messages application as well as adding a few additions to my main class

--- src/test/java/com/mycompany/mychatapp/MessagesTest.java
+++ src/test/java/com/mycompany/mychatapp/MessagesTest.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package com.mycompany.mychatapp;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author lab_services_student
- */
 public class MessagesTest {
     
     public MessagesTest() {
@@ -18,22 +10,47 @@ public class MessagesTest {
 
     @Test
     public void testCheckNumGen() {
+        int randomNum = 0;
+        Messages msTest = new Messages();
+        
+        System.out.println(msTest.checkNumGen(randomNum));
+        
     }
 
     @Test
     public void testCheckMessage() {
+        String message = "Hi Mike, are you coming tonight?";
+        Messages msTest = new Messages();
+        
+        assertTrue(msTest.checkMessage(message));
     }
 
     @Test
     public void testMenuOptions() {
+        int options = 1;
+        Messages msTest = new Messages();
+        
+        System.out.println(msTest.menuOptions(options));
     }
 
     @Test
     public void testSentMessage() {
+        String sent = "Disregard";
+        Messages msTest = new Messages();
+        
+        assertTrue(msTest.sentMessage(sent));
     }
 
     @Test
     public void testRecipientCellNo() {
+        String Rnumber = "+27647384647";
+        Messages msTest = new Messages();
+        
+        System.out.println(Rnumber);
+    }
+
+    private void assertTrue(String checkMessage) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
